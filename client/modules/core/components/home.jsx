2e2d75997df4adb68a41422e1bc335fdaf02dd69
shanyu-e <554816284@qq.com>
2016-06-23 17:28:24 +0800

    但是 这个 不起作用啊，   改的 代价 可能要自定义 继承 覆盖 组件？

--- client/modules/core/components/home.jsx
+++ client/modules/core/components/home.jsx
@@ -49,7 +49,7 @@ class Home extends React.Component {
                     title="应用ABcde"
                     style={style}
                     iconElementRight={
-                        <IconMenu style={{backgroundColor: blue400}}
+                        <IconMenu
                             iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
                             anchorOrigin={{horizontal: 'left', vertical: 'top'}}
                             targetOrigin={{horizontal: 'left', vertical: 'top'}}
