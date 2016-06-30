e7a3af6c21b680f873202be19b5878ef1ee9a21a
shanyu-e <554816284@qq.com>
2016-06-30 12:32:09 +0800

    替换为默认接口， 用域测试？

--- mobile-config.js
+++ mobile-config.js
@@ -6,7 +6,7 @@ App.info({
   description: 'A simple test app built with Meteor/Mantra/Material-UI and ios first.',
   author: 'shanyue',
   email: 'chinadengjing@gmail.com',
-  website: 'http://52.10.179.186',
+  website: 'http://oforever.net',
   version: '0.0.1',
 });
 
