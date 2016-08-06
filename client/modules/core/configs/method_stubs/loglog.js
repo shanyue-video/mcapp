61253a9dca8849c1d937cd36fdc190cb6f0a2053
shanyu-e <554816284@qq.com>
2016-08-06 23:17:37 +0800

    。。。

--- client/modules/core/configs/method_stubs/loglog.js
+++ client/modules/core/configs/method_stubs/loglog.js
@@ -2,6 +2,8 @@ export default function ({Collections, Meteor}) {
   Meteor.methods({
     'loglog.log'(text) {
       const createdAt = new Date();
+      console.log(createdAt.getTime());
+      console.log('stubs log...');
       Collections.LogLog.insert({
         text, createdAt
       });
--- server/methods/log.js
+++ server/methods/log.js
@@ -3,7 +3,9 @@ import {LogLog} from '../../lib/collections';
 export default function () {
     Meteor.methods({
         'loglog.log'(text) {
-            console.log('server log ...')
+            const ct = new Date();
+            console.log(ct.getTime());
+            console.log('server log ...');
 
             // Demo the latency compensations (Delete this in production)
             Meteor._sleepForMs(500);
