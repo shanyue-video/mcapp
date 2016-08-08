1cb6dc114a9a57c0fa97a10993f624987f65a7d8
shanyu-e <554816284@qq.com>
2016-08-08 14:29:51 +0800

    ...

--- client/modules/core/components/content.jsx
+++ client/modules/core/components/content.jsx
@@ -11,6 +11,7 @@ class Content extends React.Component {
 
     constructor(props) {
         super(props);
+        this.getChildContext = this.getChildContext.bind(this);
     }
 
     getChildContext() {
